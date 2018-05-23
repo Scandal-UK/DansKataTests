c00ae94985481c8a983175857bb1b1ff14881092
Dan Ware <scandal.uk@gmail.com>
2018-05-23 22:29:29 +0100

    “As the tests get more specific, the code gets more generic.” (constant->constant+) transformation.

--- RomanNumeralsKata/UnitTest1.cs
+++ RomanNumeralsKata/UnitTest1.cs
@@ -20,9 +20,16 @@ namespace RomanNumeralsKata
       output.Should().Be(0);
     }
 
+    [Fact]
+    public void GivenI_ShouldReturn1()
+    {
+      var output = RomanToDecimal("I");
+      output.Should().Be(1);
+    }
+
     public static int RomanToDecimal(string value)
     {
-      return 0;
+      return value == "I" ? 1 : 0;
     }
   }
 }
