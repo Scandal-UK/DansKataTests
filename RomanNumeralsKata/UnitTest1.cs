28ab91b6f955dee10cf5351abe87869dbb0d828f
Dan Ware <scandal.uk@gmail.com>
2018-05-23 22:13:31 +0100

    Second test

--- RomanNumeralsKata/UnitTest1.cs
+++ RomanNumeralsKata/UnitTest1.cs
@@ -7,9 +7,16 @@ namespace RomanNumeralsKata
   public class UnitTest1
   {
     [Fact]
-    public void Given_Null_Should_Return_Zero()
+    public void GivenNull_ShouldReturnZero()
     {
       var output = RomanToDecimal(null);
+      output.Should().Be(0);
+    }
+
+    [Fact]
+    public void GivenEmptyString_ShouldReturnZero()
+    {
+      var output = RomanToDecimal(string.Empty);
       output.Should().Be(0);
     }
 
