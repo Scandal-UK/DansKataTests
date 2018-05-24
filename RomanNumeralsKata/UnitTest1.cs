79e3de8b139d8b0e71773d187162125ec5e84c9e
Dan Ware <scandal.uk@gmail.com>
2018-05-24 23:31:15 +0100

    Refactored

--- RomanNumeralsKata/UnitTest1.cs
+++ RomanNumeralsKata/UnitTest1.cs
@@ -71,15 +71,26 @@ namespace RomanNumeralsKata
 
     public static int RomanToDecimal(string value)
     {
+      var numericValue = 0;
+      if (!string.IsNullOrEmpty(value)) 
+      {
+        numericValue = RomanCharacterValue(value[0]);
+      }
+
+      return numericValue;
+    }
+
+    private static int RomanCharacterValue(char value)
+    {
       switch (value)
       {
-        case "I": return 1;
-        case "V": return 5;
-        case "X": return 10;
-        case "L": return 50;
-        case "C": return 100;
-        case "D": return 500;
-        case "M": return 1000;
+        case 'I': return 1;
+        case 'V': return 5;
+        case 'X': return 10;
+        case 'L': return 50;
+        case 'C': return 100;
+        case 'D': return 500;
+        case 'M': return 1000;
         default: return 0;
       }
     }
