d2e430bcb021b2853d507f51b980d8b3783244d5
Dan Ware <scandal.uk@gmail.com>
2018-05-26 18:33:00 +0100

    Refactored into its own class

---
+++ RomanNumeralsKata/RomanNumeral.cs
@@ -0,0 +1,41 @@
+﻿namespace RomanNumeralsKata
+{
+  public class RomanNumeral
+  {
+    public static int NumeralToDecimal(string value)
+    {
+      var numericValue = 0;
+
+      if (!string.IsNullOrEmpty(value))
+      {
+        for (int i = 0; i < value.Length; i++)
+          numericValue += NumeralModifier(value, i);
+      }
+
+      return numericValue;
+    }
+
+    private static int NumeralModifier(string sourceNumeral, int currentIndex)
+    {
+      var currentVal = NumeralCharacterValue(sourceNumeral[currentIndex]);
+      var nextVal = (currentIndex < sourceNumeral.Length - 1) ? NumeralCharacterValue(sourceNumeral[currentIndex + 1]) : 0;
+
+      return (currentVal < nextVal) ? -currentVal : currentVal;
+    }
+
+    private static int NumeralCharacterValue(char value)
+    {
+      switch (value)
+      {
+        case 'I': return 1;
+        case 'V': return 5;
+        case 'X': return 10;
+        case 'L': return 50;
+        case 'C': return 100;
+        case 'D': return 500;
+        case 'M': return 1000;
+        default: return 0;
+      }
+    }
+  }
+}
--- RomanNumeralsKata/UnitTest1.cs
+++ RomanNumeralsKata/UnitTest1.cs
@@ -9,134 +9,98 @@ namespace RomanNumeralsKata
     [Fact]
     public void GivenNull_ShouldReturnZero()
     {
-      RomanToDecimal(null)
+      RomanNumeral.NumeralToDecimal(null)
         .Should().Be(0);
     }
 
     [Fact]
     public void GivenEmptyString_ShouldReturnZero()
     {
-      RomanToDecimal(string.Empty)
+      RomanNumeral.NumeralToDecimal(string.Empty)
         .Should().Be(0);
     }
 
     [Fact]
     public void GivenI_ShouldReturn1()
     {
-      RomanToDecimal("I")
+      RomanNumeral.NumeralToDecimal("I")
         .Should().Be(1);
     }
 
     [Fact]
     public void GivenV_ShouldReturn5()
     {
-      RomanToDecimal("V")
+      RomanNumeral.NumeralToDecimal("V")
         .Should().Be(5);
     }
 
     [Fact]
     public void GivenX_ShouldReturn10()
     {
-      RomanToDecimal("X")
+      RomanNumeral.NumeralToDecimal("X")
         .Should().Be(10);
     }
 
     [Fact]
     public void GivenL_ShouldReturn50()
     {
-      RomanToDecimal("L")
+      RomanNumeral.NumeralToDecimal("L")
         .Should().Be(50);
     }
 
     [Fact]
     public void GivenC_ShouldReturn100()
     {
-      RomanToDecimal("C")
+      RomanNumeral.NumeralToDecimal("C")
         .Should().Be(100);
     }
 
     [Fact]
     public void GivenD_ShouldReturn500()
     {
-      RomanToDecimal("D")
+      RomanNumeral.NumeralToDecimal("D")
         .Should().Be(500);
     }
 
     [Fact]
     public void GivenM_ShouldReturn1000()
     {
-      RomanToDecimal("M")
+      RomanNumeral.NumeralToDecimal("M")
         .Should().Be(1000);
     }
 
     [Fact]
     public void GivenRepeatedNumerals_ShouldReturnAddedTotal()
     {
-      RomanToDecimal("III")
+      RomanNumeral.NumeralToDecimal("III")
         .Should().Be(3);
 
-      RomanToDecimal("XX")
+      RomanNumeral.NumeralToDecimal("XX")
         .Should().Be(20);
     }
 
     [Fact]
     public void GivenDescendingNumerals_ShouldReturnAddedTotal()
     {
-      RomanToDecimal("MMVI")
+      RomanNumeral.NumeralToDecimal("MMVI")
         .Should().Be(2006);
     }
 
     [Fact]
     public void GivenAscendingNumerals_ShouldReturnSubtractedTotal()
     {
-      RomanToDecimal("IV")
+      RomanNumeral.NumeralToDecimal("IV")
         .Should().Be(4);
 
-      RomanToDecimal("CM")
+      RomanNumeral.NumeralToDecimal("CM")
         .Should().Be(900);
     }
 
     [Fact]
     public void GivenAscendingAndDescendingNumerals_ShouldReturnCorrectTotal()
     {
-      RomanToDecimal("MCMXLIV")
+      RomanNumeral.NumeralToDecimal("MCMXLIV")
         .Should().Be(1944);
-    }
-
-    public static int RomanToDecimal(string value)
-    {
-      var numericValue = 0;
-
-      if (!string.IsNullOrEmpty(value)) 
-      {
-        for (int i = 0; i < value.Length; i++)
-          numericValue += NumeralModifier(value, i);
-      }
-
-      return numericValue;
-    }
-
-    private static int NumeralModifier(string sourceNumeral, int currentIndex)
-    {
-      var currentVal = NumeralCharacterValue(sourceNumeral[currentIndex]);
-      var nextVal = (currentIndex < sourceNumeral.Length - 1) ? NumeralCharacterValue(sourceNumeral[currentIndex + 1]) : 0;
-
-      return (currentVal < nextVal) ? -currentVal : currentVal;
-    }
-
-    private static int NumeralCharacterValue(char value)
-    {
-      switch (value)
-      {
-        case 'I': return 1;
-        case 'V': return 5;
-        case 'X': return 10;
-        case 'L': return 50;
-        case 'C': return 100;
-        case 'D': return 500;
-        case 'M': return 1000;
-        default: return 0;
-      }
     }
   }
 }
