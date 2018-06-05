5f3d35f442c5790f4874730f1a81add0c0286278
Dan Ware <scandal.uk@gmail.com>
2018-06-05 21:31:32 +0100

    Passed adding two numbers. Refactored.

--- StringCalculatorKata/UnitTest1.cs
+++ StringCalculatorKata/UnitTest1.cs
@@ -18,12 +18,24 @@ namespace StringCalculatorKata
       Add("7").Should().Be(7);
     }
 
+    [Fact(DisplayName = "Given two numbers, should return sum")]
+    public void GivenTwoNumbers_ShouldReturnSum()
+    {
+      Add("1,2").Should().Be(3);
+      Add("8,5").Should().Be(13);
+    }
+
     public int Add(string numbers)
     {
       var total = 0;
 
       if (!string.IsNullOrEmpty(numbers))
-        total = int.Parse(numbers);
+      {
+        if (numbers.IndexOf(',') > 0)
+          foreach (string number in numbers.Split(','))
+            total += int.Parse(number);
+        else total = int.Parse(numbers);
+      }
 
       return total;
     }
