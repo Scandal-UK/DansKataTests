fd70f56892a8bbc74b6d8a84113de3e3997cbac3
Dan Ware <scandal.uk@gmail.com>
2018-06-07 21:25:08 +0100

    Passed test for digit 2

--- LcdDigits/LcdDigits.cs
+++ LcdDigits/LcdDigits.cs
@@ -4,24 +4,38 @@
 
   public class LcdDigits
   {
-    public static readonly string PatternFor0 =
-      "._." + Environment.NewLine +
-      "|.|" + Environment.NewLine +
-      "|_|";
+    public static readonly string[] PatternFor0 = new string[3]
+    {
+      "._.",
+      "|.|",
+      "|_|",
+    };
 
-    public static readonly string PatternFor1 =
-      "..." + Environment.NewLine +
-      "..|" + Environment.NewLine +
-      "..|";
+    public static readonly string[] PatternFor1 = new string[3]
+    {
+      "...",
+      "..|",
+      "..|",
+    };
+
+    public static readonly string[] PatternFor2 = new string[3]
+    {
+      "._.",
+      "._|",
+      "|_.",
+    };
 
     public static string Output(int number)
     {
       if (number < 0)
-      {
         throw new ArgumentException($"{nameof(number)} cannot be less than zero");
-      }
 
-      return string.Join(Environment.NewLine, (number == 1) ? PatternFor1 : PatternFor0);
+      if (number == 1)
+        return string.Join(Environment.NewLine, PatternFor1);
+      else if (number == 2)
+        return string.Join(Environment.NewLine, PatternFor2);
+      else
+        return string.Join(Environment.NewLine, PatternFor0);
     }
   }
 }
