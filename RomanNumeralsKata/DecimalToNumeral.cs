8c1e7075e4c22e50b8b43485b7e1dd7a6e5f3b5c
Dan Ware <scandal.uk@gmail.com>
2018-06-22 20:25:13 +0100

    Updated packages and added class/test for "decimal to numeral"

---
+++ RomanNumeralsKata/DecimalToNumeral.cs
@@ -0,0 +1,19 @@
+﻿namespace RomanNumeralsKata
+{
+  using System;
+
+  public class DecimalToNumeral
+  {
+    public static string ConvertDecimal(int value)
+    {
+      if (value < 0)
+        throw new Exception("Negatives not allowed");
+
+      var numeral = string.Empty;
+
+      if (value == 1) numeral = "I";
+
+      return numeral;
+    }
+  }
+}
